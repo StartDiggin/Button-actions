e769cbdfce23978c36c11b3ccd2adf5bb38d15a9
Johnny Gonzales <dimensions122@gmail.com>
2017-08-25 13:13:37 -0700

    Function/action buttons

---
+++ algo.js
@@ -0,0 +1,15 @@
+function sort(array) {
+    var x = array.length;
+    for (var i = 0; i < x - 1; i++) {
+        var y = x - 1;
+
+        for (var j = 0; j < y - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                temp = arr[j];
+                arr[j]
+            }
+        }
+    }
+}
+
+sort[4,3,5,6];
